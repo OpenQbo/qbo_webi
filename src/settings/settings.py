e9fbb1875084b4c324946a27a351be1f48acce01
OpenQbo <developers@openqbo.org>
2012-06-12 16:32:40 +0200

    Update utf8 encoding

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -14,20 +14,14 @@ class SettingsManager(TabClass):
         self.languages_names={'en':'English','es':'Español','pt':'Português','de':'Deutsch','fr':'Français','it':'Italiano'}
 
     def get_languages(self):
-#        dirList=os.listdir("lang")
-#        os.chdir("lang")
         langlist=[]
-#        for fname in langlist.glob("*.txt"):
         for fname in glob.glob("lang/*.txt"):
             langlist.append(fname[5:-4])
         langlist.sort()
-        print langlist
         return langlist
 
 
     @cherrypy.expose
     def index(self):
         all_lang=self.get_languages()
-        return self.htmlTemplate.render(language=self.language,lannames=self.languages_names,alllanguage=all_lang)
-
-
+        return self.htmlTemplate.render(language=self.language,lannames=self.languages_names,alllanguage=all_lang).encode('utf8')
