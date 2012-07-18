cccfbf5e26363aab8e71474d9ce736a3fc70863c
qboblue <qboblue@qboblue-desktop.(none)>
2012-07-18 13:56:32 +0200

    Resolved unicode problems

--- src/voiceRecognition/sendTranscription2Server.py
+++ src/voiceRecognition/sendTranscription2Server.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#Envio de file -> http://atlee.ca/software/poster/
 import urllib2
 
 import sys
 
 
-
-data="data={\"userName\":\""+sys.argv[1]+"\", \"transcription\":\""+  sys.argv[2]   +"\", \"lang\":\""+  sys.argv[3]  +"\", \"fromBot\": true  }";
-
+#transcriptionASCII=sys.argv[2].encode('ASCII')
+#print transcriptionASCII
+#transcription=sys.argv[2].decode("utf-8")
+#print transcription
+data="data=userName:"+sys.argv[1]+",transcription:"+urllib2.quote(sys.argv[2])+",lang:"+sys.argv[3]+",fromBot:true";
+#data="data={\"userName\":\""+sys.argv[1]+"\", \"transcription\":\""+  urllib2.quote(sys.argv[2])   +"\", \"lang\":\""+  sys.argv[3]  +"\", \"fromBot\": true  }";
+print "TRANSCRIPTION SENDER"
+print data
+#data=data.encode("ASCII")
+#test=urllib2.quote(data)
+#print test
+print data
 req = urllib2.Request("http://"+sys.argv[4]+":"+sys.argv[5]+"/save", data)
 f = urllib2.urlopen(req)
 response = f.read()
 
-print response
-
+#print response
